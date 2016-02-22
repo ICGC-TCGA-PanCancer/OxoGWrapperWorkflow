e8175f009008c84d69884e003531e8ed524dfdc2
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2016-02-22 17:34:47 -0500

    fixes: for path to files; java call

--- workflow/scripts/ini_generator.sh
+++ workflow/scripts/ini_generator.sh
@@ -22,7 +22,7 @@ GITname="icgc-bot"
 GITPemFile="/home/seqware/.gnos/git.pem"
 
 PATH_TO_JSON_DIR=$1
-PATH_TO_OXOG_JAR=$2
+PATH_TO_OXOG_CLASSES=$2
 
 function update_or_append
 {
@@ -34,12 +34,12 @@ function update_or_append
 	if grep -q $search_string $file ; then
 		sed -i.bak s/$search_string=.*/$search_string=$replace_string/g $file
 	else
-		echo "\n$append_string\n" >> $file
+		echo "$append_string" >> $file
 	fi
 }
 
 for f in $(ls $PATH_TO_JSON_DIR) ; do
-	result=$(java -cp $PATH_TO_OXOG_JAR com.github.seqware.INIGenerator $PATH_TO_JSON_DIR/$f)
+	result=$(java -cp $PATH_TO_OXOG_CLASSES/bin:$PATH_TO_OXOG_CLASSES/classes com.github.seqware.INIGenerator $PATH_TO_JSON_DIR/$f)
 
 	#extract filename from output:
 	ini_file_name=$(echo $result | sed 's/.*: \.\/\(.*\.INI\)/\1/g')
@@ -51,22 +51,22 @@ for f in $(ls $PATH_TO_JSON_DIR) ; do
 	#	echo "gitMoveTestMode=$gitMoveTestMode" >> $PATH_TO_OXOG_JAR/$ini_file_name
 	#fi
 	
-	update_or_append $PATH_TO_OXOG_JAR/$ini_file_name gitMoveTestMode $gitMoveTestMode
-	update_or_append $PATH_TO_OXOG_JAR/$ini_file_name storageSource $storageSource
-	update_or_append $PATH_TO_OXOG_JAR/$ini_file_name skipDownload $skipDownload 
-	update_or_append $PATH_TO_OXOG_JAR/$ini_file_name skipUpload $skipUpload
-	update_or_append $PATH_TO_OXOG_JAR/$ini_file_name refFile $refFile
-	update_or_append $PATH_TO_OXOG_JAR/$ini_file_name uploadKey $uploadKey
-	update_or_append $PATH_TO_OXOG_JAR/$ini_file_name gnosKey $gnosKey
-	update_or_append $PATH_TO_OXOG_JAR/$ini_file_name skipOxoG $skipOxoG
-	update_or_append $PATH_TO_OXOG_JAR/$ini_file_name skipVariantBam $skipVariantBam 
-	update_or_append $PATH_TO_OXOG_JAR/$ini_file_name skipAnnotation $skipAnnotation
-	update_or_append $PATH_TO_OXOG_JAR/$ini_file_name uploadURL $uploadURL
-	update_or_append $PATH_TO_OXOG_JAR/$ini_file_name JSONrepo $JSONrepo
-	update_or_append $PATH_TO_OXOG_JAR/$ini_file_name JSONrepoName $JSONrepoName
-	update_or_append $PATH_TO_OXOG_JAR/$ini_file_name JSONfolderName $JSONfolderName
-	update_or_append $PATH_TO_OXOG_JAR/$ini_file_name JSONlocation $JSONlocation
-	update_or_append $PATH_TO_OXOG_JAR/$ini_file_name GITemail $GITemail
-	update_or_append $PATH_TO_OXOG_JAR/$ini_file_name GITname $GITname
-	update_or_append $PATH_TO_OXOG_JAR/$ini_file_name GITPemFile $GITPemFile
+	update_or_append $PATH_TO_JSON_DIR/$ini_file_name gitMoveTestMode $gitMoveTestMode
+	update_or_append $PATH_TO_JSON_DIR/$ini_file_name storageSource $storageSource
+	update_or_append $PATH_TO_JSON_DIR/$ini_file_name skipDownload $skipDownload 
+	update_or_append $PATH_TO_JSON_DIR/$ini_file_name skipUpload $skipUpload
+	update_or_append $PATH_TO_JSON_DIR/$ini_file_name refFile $refFile
+	update_or_append $PATH_TO_JSON_DIR/$ini_file_name uploadKey $uploadKey
+	update_or_append $PATH_TO_JSON_DIR/$ini_file_name gnosKey $gnosKey
+	update_or_append $PATH_TO_JSON_DIR/$ini_file_name skipOxoG $skipOxoG
+	update_or_append $PATH_TO_JSON_DIR/$ini_file_name skipVariantBam $skipVariantBam 
+	update_or_append $PATH_TO_JSON_DIR/$ini_file_name skipAnnotation $skipAnnotation
+	update_or_append $PATH_TO_JSON_DIR/$ini_file_name uploadURL $uploadURL
+	update_or_append $PATH_TO_JSON_DIR/$ini_file_name JSONrepo $JSONrepo
+	update_or_append $PATH_TO_JSON_DIR/$ini_file_name JSONrepoName $JSONrepoName
+	update_or_append $PATH_TO_JSON_DIR/$ini_file_name JSONfolderName $JSONfolderName
+	update_or_append $PATH_TO_JSON_DIR/$ini_file_name JSONlocation $JSONlocation
+	update_or_append $PATH_TO_JSON_DIR/$ini_file_name GITemail $GITemail
+	update_or_append $PATH_TO_JSON_DIR/$ini_file_name GITname $GITname
+	update_or_append $PATH_TO_JSON_DIR/$ini_file_name GITPemFile $GITPemFile
 done;
