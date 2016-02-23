b7a5c7a2b00d81c5daf671ce17ce8fb51a249f28
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2016-02-23 10:18:13 -0500

    Fixed typo referencing wrong VCF ; fixed path to refdata ;

--- workflow/scripts/run_oxog_extracted_SNVs.sh
+++ workflow/scripts/run_oxog_extracted_SNVs.sh
@@ -21,16 +21,16 @@ echo "$VCF3 has $NUMINDELS3 INDELS"
 VCFFORDOCKER=""
 VCFFOROXOG=""
 if (( NUMINDELS1 > 0 )) ; then
-        VCFFORDOCKER+=" -v ${VCF1}:/VCF1.vcf.gz "
-        VCFFOROXOG+=" /VCF1.vcf.gz "
+        VCFFORDOCKER=" ${VCFFORDOCKER} -v ${VCF1}:/VCF1.vcf.gz "
+        VCFFOROXOG=" ${VCFFOROXOG} /VCF1.vcf.gz "
 fi
 if (( NUMINDELS2 > 0 )) ; then
-        VCFFORDOCKER+=" -v ${VCF2}:/VCF2.vcf.gz "
-        VCFFOROXOG+=" /VCF2.vcf.gz "
+        VCFFORDOCKER=" ${VCFFORDOCKER} -v ${VCF2}:/VCF2.vcf.gz "
+        VCFFOROXOG=" ${VCFFOROXOG} /VCF2.vcf.gz "
 fi
 if (( NUMINDELS3 > 0 )) ; then
-        VCFFORDOCKER+=" -v ${VCF2}:/VCF3.vcf.gz "
-        VCFFOROXOG+=" /VCF3.vcf.gz "
+        VCFFORDOCKER=" ${VCFFORDOCKER} -v ${VCF3}:/VCF3.vcf.gz "
+        VCFFOROXOG=" ${VCFFOROXOG} /VCF3.vcf.gz "
 fi
 
 if [[ $VCFFORDOCKER != "" ]] ; then
@@ -40,7 +40,7 @@ if [[ $VCFFORDOCKER != "" ]] ; then
 
 
     sudo docker run --rm --name="oxog_container_snv_from_indel" \
-                -v /datastore/refdata/:/cga/fh/pcawg_pipeline/refdata/ \
+                -v /refdata/:/cga/fh/pcawg_pipeline/refdata/ \
                 -v /datastore/oxog_workspace_extracted_snvs/:/cga/fh/pcawg_pipeline/jobResults_pipette/jobs/${ALIQUOTID}/:rw \
                 -v /datastore/bam/:/datafiles/BAM/ \
             	${VCFFORDOCKER} \
