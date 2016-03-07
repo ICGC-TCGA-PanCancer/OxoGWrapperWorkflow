4c08a3e905884114f5bddfcff8ac69e0e36bd4c6
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2016-03-07 15:51:56 -0500

    Fix call to sed ; add DEST_DIR

--- workflow/scripts/ini_generator.sh
+++ workflow/scripts/ini_generator.sh
@@ -23,16 +23,18 @@ GITPemFile="/datastore/credentials/git.pem"
 
 PATH_TO_JSON_DIR=$1
 PATH_TO_OXOG_CLASSES=$2
+DEST_DIR=$3
 
 function update_or_append
 {
-	file=$1
+	echo "searching in $1 for $2 and replacing with $3"
+    file=$1
 	search_string=$2
 	replace_string=$3
 	append_string="$search_string=$replace_string"
-	
+
 	if grep -q $search_string $file ; then
-		sed -i.bak s/$search_string=.*/$search_string=$replace_string/g $file
+		sed -i.bak s/$search_string\ *=.*/$search_string=${replace_string//\//\\\/}/g $file
 	else
 		echo "$append_string" >> $file
 	fi
@@ -43,30 +45,34 @@ for f in $(ls $PATH_TO_JSON_DIR) ; do
 
 	#extract filename from output:
 	ini_file_name=$(echo $result | sed 's/.*: \.\/\(.*\.INI\)/\1/g')
-	
+
 	echo "processing $ini_file_name..."
-	#if grep -q gitMoveTestMode $PATH_TO_OXOG_JAR/$ini_file_name ; then
-	#	sed -i.bak s/gitMoveTestMode=.*/$gitMoveTestMode/g $PATH_TO_OXOG_JAR/$ini_file_name
-	#else
-	#	echo "gitMoveTestMode=$gitMoveTestMode" >> $PATH_TO_OXOG_JAR/$ini_file_name
-	#fi
-	
-	update_or_append $PATH_TO_JSON_DIR/$ini_file_name gitMoveTestMode $gitMoveTestMode
-	update_or_append $PATH_TO_JSON_DIR/$ini_file_name storageSource $storageSource
-	update_or_append $PATH_TO_JSON_DIR/$ini_file_name skipDownload $skipDownload 
-	update_or_append $PATH_TO_JSON_DIR/$ini_file_name skipUpload $skipUpload
-	update_or_append $PATH_TO_JSON_DIR/$ini_file_name refFile $refFile
-	update_or_append $PATH_TO_JSON_DIR/$ini_file_name uploadKey $uploadKey
-	update_or_append $PATH_TO_JSON_DIR/$ini_file_name gnosKey $gnosKey
-	update_or_append $PATH_TO_JSON_DIR/$ini_file_name skipOxoG $skipOxoG
-	update_or_append $PATH_TO_JSON_DIR/$ini_file_name skipVariantBam $skipVariantBam 
-	update_or_append $PATH_TO_JSON_DIR/$ini_file_name skipAnnotation $skipAnnotation
-	update_or_append $PATH_TO_JSON_DIR/$ini_file_name uploadURL $uploadURL
-	update_or_append $PATH_TO_JSON_DIR/$ini_file_name JSONrepo $JSONrepo
-	update_or_append $PATH_TO_JSON_DIR/$ini_file_name JSONrepoName $JSONrepoName
-	update_or_append $PATH_TO_JSON_DIR/$ini_file_name JSONfolderName $JSONfolderName
-	update_or_append $PATH_TO_JSON_DIR/$ini_file_name JSONlocation $JSONlocation
-	update_or_append $PATH_TO_JSON_DIR/$ini_file_name GITemail $GITemail
-	update_or_append $PATH_TO_JSON_DIR/$ini_file_name GITname $GITname
-	update_or_append $PATH_TO_JSON_DIR/$ini_file_name GITPemFile $GITPemFile
+    #if grep -q gitMoveTestMode $PATH_TO_OXOG_JAR/$ini_file_name ; then
+    #       sed -i.bak s/gitMoveTestMode=.*/$gitMoveTestMode/g $PATH_TO_OXOG_JAR/$ini_file_name
+    #else
+    #       echo "gitMoveTestMode=$gitMoveTestMode" >> $PATH_TO_OXOG_JAR/$ini_file_name
+    #fi
+	mv $ini_file_name $DEST_DIR/$ini_file_name
+	update_or_append $DEST_DIR/$ini_file_name gitMoveTestMode $gitMoveTestMode
+	update_or_append $DEST_DIR/$ini_file_name storageSource $storageSource
+	update_or_append $DEST_DIR/$ini_file_name skipDownload $skipDownload
+	update_or_append $DEST_DIR/$ini_file_name skipUpload $skipUpload
+	#update_or_append $DEST_DIR/$ini_file_name refFile $refFile
+	update_or_append $DEST_DIR/$ini_file_name uploadKey $uploadKey
+	update_or_append $DEST_DIR/$ini_file_name gnosKey $gnosKey
+	update_or_append $DEST_DIR/$ini_file_name skipOxoG $skipOxoG
+	update_or_append $DEST_DIR/$ini_file_name skipVariantBam $skipVariantBam
+	update_or_append $DEST_DIR/$ini_file_name skipAnnotation $skipAnnotation
+	update_or_append $DEST_DIR/$ini_file_name uploadURL $uploadURL
+	update_or_append $DEST_DIR/$ini_file_name JSONrepo $JSONrepo
+	update_or_append $DEST_DIR/$ini_file_name JSONrepoName $JSONrepoName
+	update_or_append $DEST_DIR/$ini_file_name JSONfolderName $JSONfolderName
+	update_or_append $DEST_DIR/$ini_file_name JSONlocation $JSONlocation
+	update_or_append $DEST_DIR/$ini_file_name GITemail $GITemail
+	update_or_append $DEST_DIR/$ini_file_name GITname $GITname
+	update_or_append $DEST_DIR/$ini_file_name GITPemFile $GITPemFile
+	update_or_append $DEST_DIR/$ini_file_name studyRefNameOverride $studyRefNameOverride
+	update_or_append $DEST_DIR/$ini_file_name svPadding $svPadding
+	update_or_append $DEST_DIR/$ini_file_name snvPadding $snvPadding
+	update_or_append $DEST_DIR/$ini_file_name indelPadding $indelPadding
 done;
