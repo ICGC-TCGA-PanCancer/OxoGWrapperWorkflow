8b2f8c9dba18e3176b06a94e8d3abe55313ceffd
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2016-03-14 17:32:55 -0400

    Fix file name and missing \"

--- src/main/java/com/github/seqware/GNOSDownloader.java
+++ src/main/java/com/github/seqware/GNOSDownloader.java
@@ -12,10 +12,10 @@ public class GNOSDownloader implements WorkflowFileDownloader {
 	public String getDownloadCommandString(String downloadDir, String workflowName, String ... objectIDs) {
 
 		String getFilesCommand = "( docker run --rm --name get_vcf_"+workflowName+" "
-				+ " -v /datastore/credentials/gnos.pem:/gnos.pem "
+				+ " -v /datastore/credentials/gnos.key:/gnos.key "
 			    + " -v "+downloadDir+"/:/downloads/:rw"
 	    		+ " pancancer/pancancer_upload_download:1.7 /bin/bash -c \""
-	    			+ "gtdownload -k 30 --peer-timeout 120 -p /downloads/ -l /downloads/gtdownload.log -c /gnos.pem "+objectIDs[0]+") ";
+	    			+ "gtdownload -k 30 --peer-timeout 120 -p /downloads/ -l /downloads/gtdownload.log -c /gnos.key "+objectIDs[0]+" \" ) ";
 
 		return getFilesCommand;
 	}
