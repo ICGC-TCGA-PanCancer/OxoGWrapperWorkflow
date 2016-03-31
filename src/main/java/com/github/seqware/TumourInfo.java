b6c8e19a0b3d5009b1e487eeef5d0bb09592707c
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2016-03-31 15:23:34 -0400

    New class to store tumour info - makes it easier to handle multitumours

---
+++ src/main/java/com/github/seqware/TumourInfo.java
@@ -0,0 +1,76 @@
+package com.github.seqware;
+
+class TumourInfo {
+	String tumourMetdataURL;
+	String tumourBAMFileName;
+	String tumourBamGnosID;
+	String tumourMinibamPath;
+	String tumourBamGNOSRepoURL;
+	String tumourBamIndexFileName;
+	String bamTumourIndexObjectID;
+	String bamTumourObjectID;
+
+	public String getTumourMetdataURL() {
+		return this.tumourMetdataURL;
+	}
+
+	public void setTumourMetdataURL(String tumourMetdataURL) {
+		this.tumourMetdataURL = tumourMetdataURL;
+	}
+
+	public String getTumourBAMFileName() {
+		return this.tumourBAMFileName;
+	}
+
+	public void setTumourBAMFileName(String tumourBAMFileName) {
+		this.tumourBAMFileName = tumourBAMFileName;
+	}
+
+	public String getTumourBamGnosID() {
+		return this.tumourBamGnosID;
+	}
+
+	public void setTumourBamGnosID(String tumourBamGnosID) {
+		this.tumourBamGnosID = tumourBamGnosID;
+	}
+
+	public String getTumourMinibamPath() {
+		return this.tumourMinibamPath;
+	}
+
+	public void setTumourMinibamPath(String tumourMinibamPath) {
+		this.tumourMinibamPath = tumourMinibamPath;
+	}
+
+	public String getTumourBamGNOSRepoURL() {
+		return this.tumourBamGNOSRepoURL;
+	}
+
+	public void setTumourBamGNOSRepoURL(String tumourBamGNOSRepoURL) {
+		this.tumourBamGNOSRepoURL = tumourBamGNOSRepoURL;
+	}
+
+	public String getTumourBamIndexFileName() {
+		return this.tumourBamIndexFileName;
+	}
+
+	public void setTumourBamIndexFileName(String tumourBamIndexFileName) {
+		this.tumourBamIndexFileName = tumourBamIndexFileName;
+	}
+
+	public String getBamTumourObjectID() {
+		return this.bamTumourObjectID;
+	}
+
+	public void setBamTumourObjectID(String bamTumourObjectID) {
+		this.bamTumourObjectID = bamTumourObjectID;
+	}
+
+	public String getBamTumourIndexObjectID() {
+		return this.bamTumourIndexObjectID;
+	}
+
+	public void setBamTumourIndexObjectID(String bamTumourIndexObjectID) {
+		this.bamTumourIndexObjectID = bamTumourIndexObjectID;
+	}
+}
