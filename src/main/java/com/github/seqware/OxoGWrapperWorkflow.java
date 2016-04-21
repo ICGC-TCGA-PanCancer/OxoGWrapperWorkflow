37c28eb8ea9f0f2fc96f8fcb8c9fc1171ee539a7
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2016-04-21 11:09:08 -0400

    Code cleanup.

--- src/main/java/com/github/seqware/OxoGWrapperWorkflow.java
+++ src/main/java/com/github/seqware/OxoGWrapperWorkflow.java
@@ -394,33 +394,6 @@ public class OxoGWrapperWorkflow extends BaseOxoGWrapperWorkflow {
 	 * @return
 	 */
 	private Job doUpload(Job parentJob) {
-//		String moveToFailed = GitUtils.gitMoveCommand("uploading-jobs","failed-jobs",this.JSONlocation + "/" + this.JSONrepoName + "/" + this.JSONfolderName,this.JSONfileName, this.gitMoveTestMode, this.getWorkflowBaseDir() + "/scripts/");
-//		// Will need to run gtupload to generate the analysis.xml and manifest files (but not actually upload). 
-//		Job generateAnalysisFilesVCFs = generateVCFMetadata(parentJob, moveToFailed);
-//		
-//		Job generateAnalysisFilesBAMs = generateBAMMetadata(parentJob, moveToFailed);
-//	
-//		String gnosServer = this.gnosMetadataUploadURL.replace("http://", "").replace("https://", "").replace("/", "");
-//		//Note: It was decided there should be two uploads: one for minibams and one for VCFs (for people who want VCFs but not minibams).
-//		Job uploadVCFResults = this.getWorkflow().createBashJob("upload VCF results");
-//		String uploadVCFCommand = "sudo chmod 0600 /datastore/credentials/rsync.key\n"
-//								+ "UPLOAD_PATH=$( echo \""+this.uploadURL+"\" | sed 's/\\(.*\\)\\:\\(.*\\)/\\2/g' )\n"
-//								+ "VCF_UUID=$(grep server_path /datastore/files_for_upload/manifest.xml  | sed 's/.*server_path=\\\"\\(.*\\)\\\" .*/\\1/g')\n"
-//								+ "( rsync -avtuz -e 'ssh -o UserKnownHostsFile=/datastore/credentials/known_hosts -o IdentitiesOnly=yes -o BatchMode=yes -o PasswordAuthentication=no -o PreferredAuthentications=publickey -i "+this.uploadKey+"'"
-//										+ " --rsync-path=\"mkdir -p $UPLOAD_PATH/"+gnosServer+"/$VCF_UUID && rsync\" /datastore/files_for_upload/ " + this.uploadURL+ "/"+gnosServer + "/$VCF_UUID ) ";
-//		uploadVCFCommand += (" || " + moveToFailed);
-//		uploadVCFResults.setCommand(uploadVCFCommand);
-//		uploadVCFResults.addParent(generateAnalysisFilesVCFs);
-//		
-//		Job uploadBAMResults = this.getWorkflow().createBashJob("upload BAM results");
-//		String uploadBAMcommand = "sudo chmod 0600 /datastore/credentials/rsync.key\n"
-//								+ "UPLOAD_PATH=$( echo \""+this.uploadURL+"\" | sed 's/\\(.*\\)\\:\\(.*\\)/\\2/g' )\n"
-//								+ "BAM_UUID=$(grep server_path /datastore/variantbam_results/manifest.xml  | sed 's/.*server_path=\\\"\\(.*\\)\\\" .*/\\1/g')\n"
-//								+ "( rsync -avtuz -e 'ssh -o UserKnownHostsFile=/datastore/credentials/known_hosts -o IdentitiesOnly=yes -o BatchMode=yes -o PasswordAuthentication=no -o PreferredAuthentications=publickey -i "+this.uploadKey+"'"
-//										+ " --rsync-path=\"mkdir -p $UPLOAD_PATH/"+gnosServer+"/$BAM_UUID && rsync\" /datastore/variantbam_results/ " + this.uploadURL+ "/"+gnosServer + "/$BAM_UUID ) ";
-//		uploadBAMcommand += (" || " + moveToFailed);
-//		uploadBAMResults.setCommand(uploadBAMcommand);
-//		uploadBAMResults.addParent(generateAnalysisFilesBAMs);
 		
 		UploadJobGenerator generator = new UploadJobGenerator();
 		generator.setJSONFileInfo(this.JSONlocation, this.JSONrepoName, this.JSONfolderName, this.JSONfileName);
@@ -442,121 +415,10 @@ public class OxoGWrapperWorkflow extends BaseOxoGWrapperWorkflow {
 		generator.setWorkflowSourceURL(this.workflowSourceURL);
 		generator.setWorkflowURL(this.workflowURL);
 		
-		//uploadBAMResults.addParent(uploadVCFResults);
 		List<String> vcfsForUpload = this.filesForUpload.stream().filter(p -> ((p.contains(".vcf") || p.endsWith(".tar")) && !( p.contains("SNVs_from_INDELs") || p.contains("extracted-snv"))) ).distinct().collect(Collectors.toList());
 		List<String> bamFilesForUpload = this.filesForUpload.stream().filter( p -> p.contains(".bam") || p.contains(".bai") ).distinct().collect(Collectors.toList());
 		return generator.doUpload(this, parentJob, bamFilesForUpload, vcfsForUpload);
 	}
-//
-//	private Job generateBAMMetadata(Job parentJob, String moveToFailed) {
-//		Job generateAnalysisFilesBAMs = this.getWorkflow().createBashJob("generate_analysis_files_for_BAM_upload");
-//		
-//		String bams = "";
-//		String bamIndicies = "";
-//		String bamMD5Sums = "";
-//		String bamIndexMD5Sums = "";
-//		String generateAnalysisFilesBAMsCommand = "";
-//		generateAnalysisFilesBAMsCommand += "sudo chmod a+rw -R /datastore/variantbam_results/ &&\n";
-//		//I don't think distinct() should be necessary.
-//		for (String file : this.filesForUpload.stream().filter( p -> p.contains(".bam") || p.contains(".bai") ).distinct().collect(Collectors.toList()) )
-//		{
-//			file = file.trim();
-//			//md5sum test_files/tumour_minibam.bam.bai | cut -d ' ' -f 1 > test_files/tumour_minibam.bai.md5
-//			generateAnalysisFilesBAMsCommand += " md5sum "+file+" | cut -d ' ' -f 1 > "+file+".md5 ; \n";
-//			
-//			if (file.contains(".bai") )
-//			{
-//				bamIndicies += file + ",";
-//				bamIndexMD5Sums += file + ".md5" + ",";
-//			}
-//			else
-//			{
-//				bams += file + ",";
-//				bamMD5Sums += file + ".md5" + ",";
-//			}
-//		}
-//		String descriptionEnd = TemplateUtils.getRenderedTemplate("analysisDescriptionSuffix.template");
-//		String bamDescription = TemplateUtils.getRenderedTemplate(Arrays.stream(new String [][] {
-//				{"donorID", this.donorID}, {"specimenID", this.specimenID}, {"snvPadding", String.valueOf(this.snvPadding)},
-//				{"indelPadding", String.valueOf(this.indelPadding)}, {"svPadding", String.valueOf(this.svPadding)},
-//				{"workflowName", this.getName()}, {"workflowVersion", this.getVersion()}, {"workflowURL", this.workflowURL},
-//				{"workflowSrcURL", this.workflowSourceURL}, {"changeLogURL", this.changelogURL}, {"descriptionSuffix", descriptionEnd}
-//			}).collect(this.collectToMap), "analysisBAMDescription.template");
-//		generateAnalysisFilesBAMsCommand += TemplateUtils.getRenderedTemplate(Arrays.stream(new String[][] {
-//				{ "gnosKey", this.gnosKey }, { "gnosMetadataUploadURL", this.gnosMetadataUploadURL }, { "bamDescription", bamDescription },
-//				{ "normalMetadataURL", this.normalMetdataURL } , { "tumourMetadataURLs", String.join("," , this.tumours.stream().map(t -> t.getTumourMetdataURL()).collect(Collectors.toList()) ) },
-//				{ "bams", bams }, { "bamIndicies", bamIndicies}, { "bamMD5Sums", bamMD5Sums }, { "bamIndexMD5Sums", bamIndexMD5Sums}, 
-//				{ "studyRefNameOverride", this.studyRefNameOverride }, { "workflowVersion", this.getVersion() } 
-//			}).collect(this.collectToMap),"generateBAMAnalysisMetadata.template");
-//
-//		generateAnalysisFilesBAMs.setCommand("( "+generateAnalysisFilesBAMsCommand+" ) || "+moveToFailed);
-//		generateAnalysisFilesBAMs.addParent(parentJob);
-//		return generateAnalysisFilesBAMs;
-//	}
-//
-//	private Job generateVCFMetadata(Job parentJob, String moveToFailed) {
-//		Job generateAnalysisFilesVCFs = this.getWorkflow().createBashJob("generate_analysis_files_for_VCF_upload");
-//		//Files need to be copied to the staging directory
-//		String vcfs = "";
-//		String vcfIndicies = "";
-//		String vcfMD5Sums = "";
-//		String vcfIndexMD5Sums = "";
-//		
-//		String tars = "";
-//		String tarMD5Sums = "";
-//		
-//		String generateAnalysisFilesVCFCommand = "";
-//		//I don't think "distinct" should be necessary here, but there were weird duplicates popping up in the list.
-//		for (String file : this.filesForUpload.stream().filter(p -> ((p.contains(".vcf") || p.endsWith(".tar")) && !( p.contains("SNVs_from_INDELs") || p.contains("extracted-snv"))) ).distinct().collect(Collectors.toList()) )
-//		{
-//			file = file.trim();
-//			//md5sum test_files/tumour_minibam.bam.bai | cut -d ' ' -f 1 > test_files/tumour_minibam.bai.md5
-//			generateAnalysisFilesVCFCommand += "md5sum "+file+" | cut -d ' ' -f 1 > "+file+".md5 ; \n";
-//			
-//			if (file.endsWith(".tar"))
-//			{
-//				tars += file + ",";
-//				tarMD5Sums += file+".md5,";
-//			}
-//			else if (file.contains(".tbi") || file.contains(".idx"))
-//			{
-//				vcfIndicies += file + ",";
-//				vcfIndexMD5Sums += file + ".md5," ;
-//			}
-//			else
-//			{
-//				vcfs += file + ",";
-//				vcfMD5Sums += file + ".md5,";	 
-//			}
-//		}
-//		// trim trailing commas so that you don't get ",," in the "--vcfs ..." arg to gt-download-upload-wrapper
-//		// since that will result in empty files in the analysis and things will break.
-//		vcfs = vcfs.substring(0,vcfs.length()-1);
-//		vcfMD5Sums = vcfMD5Sums.substring(0,vcfMD5Sums.length()-1);
-//		vcfIndicies = vcfIndicies.substring(0,vcfIndicies.length()-1);
-//		vcfIndexMD5Sums = vcfIndexMD5Sums.substring(0,vcfIndexMD5Sums.length()-1);
-//		
-//		String descriptionEnd = TemplateUtils.getRenderedTemplate("analysisDescriptionSuffix.template");
-//		String vcfDescription = TemplateUtils.getRenderedTemplate(Arrays.stream(new String[][]{
-//				{ "OxoQScore",this.oxoQScore },					{ "donorID",this.donorID },					{ "specimenID",this.specimenID },
-//				{ "workflowName",this.getName() },				{ "workflowVersion",this.getVersion() },	{ "workflowURL",this.workflowURL },
-//				{ "workflowSrcURL",this.workflowSourceURL },	{ "changeLogURL",this.changelogURL },		{ "descriptionSuffix",descriptionEnd },
-//			}).collect(this.collectToMap), "analysisVCFDescription.template");
-//
-//		
-//		generateAnalysisFilesVCFCommand += TemplateUtils.getRenderedTemplate(Arrays.stream(new String[][] {
-//				{ "gnosKey", this.gnosKey }, { "gnosMetadataUploadURL", this.gnosMetadataUploadURL }, { "vcfDescription", vcfDescription },
-//				{ "normalMetadataURL", this.normalMetdataURL } , { "tumourMetadataURLs", String.join("," , this.tumours.stream().map(t -> t.getTumourMetdataURL()).collect(Collectors.toList()) ) },
-//				{ "vcfs", vcfs }, { "tars", tars }, { "tarMD5Sums", tarMD5Sums }, { "vcfIndicies", vcfIndicies}, { "vcfMD5Sums", vcfMD5Sums },
-//				{ "vcfIndexMD5Sums", vcfIndexMD5Sums}, { "studyRefNameOverride", this.studyRefNameOverride }, { "workflowVersion", this.getVersion() } 
-//			}).collect(this.collectToMap),"generateVCFAnalysisMetadata.template");
-//		generateAnalysisFilesVCFs.setCommand("( "+generateAnalysisFilesVCFCommand+ " ) || "+moveToFailed);
-//		generateAnalysisFilesVCFs.addParent(parentJob);
-//		return generateAnalysisFilesVCFs;
-//	}
-//
-
-	
 
 	/*
 	 * Wrapper function to GitUtils.giveMove 
