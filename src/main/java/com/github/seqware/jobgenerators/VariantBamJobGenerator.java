c02450e80e4c05224e9b5fa36135e020a0420284
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2016-07-26 10:05:38 -0400

    Fixed issues with file name extensions.

--- src/main/java/com/github/seqware/jobgenerators/VariantBamJobGenerator.java
+++ src/main/java/com/github/seqware/jobgenerators/VariantBamJobGenerator.java
@@ -41,35 +41,35 @@ public class VariantBamJobGenerator extends JobGeneratorBase{
 		String oldMinibamName = "";
 		if (bamType == BAMType.normal)
 		{
-			oldMinibamName = bamName.replace(".bam", "_minibam");
-			minibamName = this.aliquotID + ".normal.variantbam." + LocalDate.now().format(DateTimeFormatter.BASIC_ISO_DATE);
+			oldMinibamName = (bamName.replace(".bam", "_minibam"))+".bam";
+			minibamName = this.aliquotID + ".normal.variantbam." + LocalDate.now().format(DateTimeFormatter.BASIC_ISO_DATE) + ".bam";
 			String normalMinibamPath = "/datastore/variantbam_results/"+minibamName;
 			String oldMinibamPath = "/datastore/variantbam_results/"+oldMinibamName;
-			updateFilesForUpload.accept(normalMinibamPath+".bam",null,false);
+			updateFilesForUpload.accept(normalMinibamPath,null,false);
 			updateFilesForUpload.accept(normalMinibamPath+".bai",null,false);
-			updateFilesForUpload.accept(oldMinibamPath+".bam",null,true);
+			updateFilesForUpload.accept(oldMinibamPath,null,true);
 			updateFilesForUpload.accept(oldMinibamPath+".bai",null,true);
 		}
 		else
 		{
 			oldMinibamName = tumourBAMFileName.replace(".bam", "_minibam");
-			minibamName = this.aliquotID + ".tumour.variantbam." + LocalDate.now().format(DateTimeFormatter.BASIC_ISO_DATE);
+			minibamName = this.aliquotID + ".tumour.variantbam." + LocalDate.now().format(DateTimeFormatter.BASIC_ISO_DATE) + ".bam";
 			String tumourMinibamPath = "/datastore/variantbam_results/"+minibamName;
 			String oldMinibamPath = "/datastore/variantbam_results/"+oldMinibamName;
-			updateFilesForUpload.accept(tumourMinibamPath+".bam",aliquotID,false);
+			updateFilesForUpload.accept(tumourMinibamPath,aliquotID,false);
 			updateFilesForUpload.accept(tumourMinibamPath+".bai",aliquotID,false);
-			updateFilesForUpload.accept(oldMinibamPath+".bam",aliquotID,true);
+			updateFilesForUpload.accept(oldMinibamPath,aliquotID,true);
 			updateFilesForUpload.accept(oldMinibamPath+".bai",aliquotID,true);
 		}
 		
 		String command = TemplateUtils.getRenderedTemplate(Arrays.stream( new String[][] {
 			{ "containerNameSuffix", bamType + (bamType == BAMType.tumour ? "_with_tumour_"+this.aliquotID : "") },
-			{ "minibamName", minibamName+".bam"},  {"snvPadding", String.valueOf(this.snvPadding)}, {"svPadding", String.valueOf(this.svPadding)},
+			{ "minibamName", minibamName },  {"snvPadding", String.valueOf(this.snvPadding)}, {"svPadding", String.valueOf(this.svPadding)},
 			{ "indelPadding", String.valueOf(this.indelPadding) }, { "pathToBam", bamPath },
 			{ "snvVcf", snvVcf }, { "svVcf", svVcf }, { "indelVcf", indelVcf }
 		}).collect(this.collectToMap), "runVariantbam.template" );
 
-		String linkWithOldNamingConvention = "cd /datastore/variantbam_results && ln -sf "+minibamName+".bam "+oldMinibamName + ".bam && ln -sf "+minibamName+".bam.bai "+oldMinibamName+".bam.bai" ;
+		String linkWithOldNamingConvention = "cd /datastore/variantbam_results && ln -sf "+minibamName+" "+oldMinibamName + " && ln -sf "+minibamName+".bai "+oldMinibamName+".bai" ;
 		String moveToFailed = GitUtils.gitMoveCommand("running-jobs","failed-jobs",this.JSONlocation + "/" + this.JSONrepoName + "/" + this.JSONfolderName,this.JSONfileName, this.gitMoveTestMode, workflow.getWorkflowBaseDir() + "/scripts/");
 		command = ( "(" + command + " && " + linkWithOldNamingConvention + " ) || " + moveToFailed);
 		runVariantbam.setCommand(command);
